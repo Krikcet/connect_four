07fbb145c6c473612c0837fbc8e20aa237075fe6
Jacob Logue <jakelogue84@gmail.com>
2021-12-23 12:49:46 -0500

    add display board method

--- lib/board.rb
+++ lib/board.rb
@@ -2,13 +2,20 @@
 
 # create board object for connect four
 class Board
-  attr_reader :grid
+  attr_accessor :grid
+  attr_reader :rows, :columns
 
   def initialize(rows: 6, columns: 7)
+    @rows = rows
+    @columns = columns
     @grid = create_grid(rows, columns)
   end
 
   def create_grid(rows, columns)
     Array.new(rows, Array.new(columns, ' '))
+  end
+
+  def display_board
+    grid.each { |row| p row }
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative '../lib/board'
+require_relative '../lib/player'
+
 # create game instance of connect four
 class Game
   attr_reader :player_one, :player_two
@@ -11,5 +14,5 @@ class Game
     @player_two = Player.new('O')
   end
 
-  
+
 end
